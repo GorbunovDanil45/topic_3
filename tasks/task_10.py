e97fd811a018a8abf09853bac9a49e34d444ef42
Gorbunov Danil <danil45@bk.ru>
2025-04-27 21:17:32 +0500

    задача 10

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -1,7 +1,7 @@
-name = ...  # здесь Ваш код
-surname = ...  # здесь Ваш код
-age = ...  # здесь Ваш код
+name = input('Введите ваше имя:')
+surname = input('Введите вашу фамилию:')
+age = int(input('Введите ваш возраст:'))
 
-print(...)  # допишите код
-print(...)  # допишите код
-print(...)  # допишите код
+print('Тип имени:', type(name), 'ID в памяти:', id(name))
+print('Тип фамилии:', type(surname), 'ID в памяти:', id(surname))
+print('Тип возраста:', type(age), 'ID в памяти:', id(age))
